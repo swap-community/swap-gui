321836909f3bec7e303409adc2dd0a8548b55137
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2019-03-22 17:25:36 -0400

    reduce datadir requirement

--- src/daemon/DaemonManager.cpp
+++ src/daemon/DaemonManager.cpp
@@ -305,9 +305,9 @@ QVariantMap DaemonManager::validateDataDir(const QString &dataDir) const
             valid = false;
         }
 
-        // Make sure there is 75GB storage available
+        // Make sure there is 20GB storage available
         storageAvailable = storage.bytesAvailable()/1000/1000/1000;
-        if (storageAvailable < 75) {
+        if (storageAvailable < 20) {
             valid = false;
         }
     } else {
