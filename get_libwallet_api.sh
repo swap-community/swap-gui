239d617d16cb4c8a4a12459d44a2600b6b82354d
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2019-03-22 17:22:19 -0400

    set swap checkout branch to master

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout v0.14.1.0
+git -C $MONERO_DIR checkout origin/master
 
 # get swap core tag
 pushd $MONERO_DIR
